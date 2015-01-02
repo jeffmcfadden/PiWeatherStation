e3a16c53b6eac689e662e0108e049f26fabc8b3f
Jeff McFadden <jeff@forgeapps.com>
2015-01-02 23:05:38 +0000

    Update git ignore and old style.

--- app/controllers/old_style_controller.rb
+++ app/controllers/old_style_controller.rb
@@ -5,25 +5,29 @@ class OldStyleController < ApplicationController
   end
 
   def current_conditions
-    output = `sudo rht03_and_exit`
+    # output = `sudo rht03_and_exit`
+
+    output = `cat /sys/bus/w1/devices/28-041467b7a4ff/w1_slave`
+    cRaw = output.split( "\n" )[1].split( "t=" )[1].to_i
+    temp_c = cRaw / 1000.0
+
 
     # output = `echo "hi"`
 
-    values = output.split( "," )
+    # temp_c = values[0].split( ":" )[1].strip.to_f
+    # rh   = values[1].split( ":" )[1].gsub( '%', '' ).strip.to_f
 
-    temp_c = values[0].split( ":" )[1].strip.to_f
-    rh   = values[1].split( ":" )[1].gsub( '%', '' ).strip.to_f
-
-    tn = temp_c > 0 ? 243.12 : 272.62
-     m = temp_c > 0 ? 17.62 : 22.46
-    dew_point = tn * ( Math.log( rh / 100.0 ) + ( m * temp_c ) / ( tn + temp_c ) ) / ( m - Math.log( rh / 100.0 ) - ( m * temp_c ) / ( tn + temp_c ) )
+    # tn = temp_c > 0 ? 243.12 : 272.62
+    # m = temp_c > 0 ? 17.62 : 22.46
+    # dew_point = tn * ( Math.log( rh / 100.0 ) + ( m * temp_c ) / ( tn + temp_c ) ) / ( m - Math.log( rh / 100.0 ) - ( m * temp_c ) / ( tn + temp_c ) )
 
     # [{"name":"Temperature","value":55.4},{"name":"Humidity","value":23.8},{"name":"Dewpoint","value":18.859867363465092}]
 
     temp_f = c_to_f( temp_c.to_f )
-    dew_point_f = c_to_f( dew_point )
+    # dew_point_f = c_to_f( dew_point )
 
-    render json: [{ name: "Temperature", value: temp_f }, { name: "Humidity", value: rh }, { name: "Dewpoint", value: dew_point_f } ]
+    # render json: [{ name: "Temperature", value: temp_f }, { name: "Humidity", value: rh }, { name: "Dewpoint", value: dew_point_f } ]
+    render json: [{ name: "Temperature", value: temp_f }]
   end
 
 end
